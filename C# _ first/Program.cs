768dceefb2f50f419729e7408cf715e49e03d336
Leonardo Fortes <marcogaliofortesa@gmail.com>
2023-10-17 14:30:10 -0300

    1h25m - C#

--- C# _ first/Program.cs
+++ C# _ first/Program.cs
@@ -1,18 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Using = usando // Serve para adicionar modulo ao código 
+//Modulos
 
-namespace C____first
+using System; // Modúlos / Serve para que o C# seja capaz de manipular coisas do sistema operacional do usuario (data, console, etc)
+using System.Collections.Generic; // Modúlos / 
+using System.Linq; // Modúlos / Trabalhar com um conjunto muito grande de dados
+using System.Text; // Modúlos / Permite que você manipule texto (minusculo, maiusculo, remover espaços em branco, etc)
+using System.Threading.Tasks; // Modúlos / Serve para ao criar um programa que roda em varios nucleos de processadores 
+
+//Inicio do programa
+namespace C____first // Nome do código fonte 
 {
-    internal class Program
+    internal class Program // POO
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // Função Principal (Todo código colocado dentro da função "MAIN" é o primeiro código que será executado;
         {
-            Console.WriteLine "Hello World"
+            Console.WriteLine("Test_c#_first_program");
+            Console.WriteLine("Test_C#_repository");
+            Console.Write("Quebra_de_linha\n");
+            Console.Write("Test");
 
 
+            Console.ReadLine();
         }
     }
 }
